24c620a8b6963e945178fe6da67ecfd74ebcdd0f
Michael Lepecq <lepecqmichael@gmail.com>
2018-05-21 16:41:00 -0400

    add models validation and created an empty vehicule controller

---
+++ db/migrate/20180521201545_create_bookings.rb
@@ -0,0 +1,13 @@
+class CreateBookings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bookings do |t|
+      t.date :check_in
+      t.date :check_out
+      t.references :vehicule, foreign_key: true
+      t.references :user, foreign_key: true
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_21_194430) do
+ActiveRecord::Schema.define(version: 2018_05_21_201545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "check_in"
+    t.date "check_out"
+    t.bigint "vehicule_id"
+    t.bigint "user_id"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+    t.index ["vehicule_id"], name: "index_bookings_on_vehicule_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -49,5 +61,7 @@ ActiveRecord::Schema.define(version: 2018_05_21_194430) do
     t.index ["user_id"], name: "index_vehicules_on_user_id"
   end
 
+  add_foreign_key "bookings", "users"
+  add_foreign_key "bookings", "vehicules"
   add_foreign_key "vehicules", "users"
 end
