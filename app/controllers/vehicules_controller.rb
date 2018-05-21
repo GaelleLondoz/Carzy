5a26b3d3161d28826559d46ca1e62c7ddf8b23f2
joy navi <joy.navi@gmail.com>
2018-05-21 18:02:12 -0400

    Vehicule controller index

--- app/controllers/vehicules_controller.rb
+++ app/controllers/vehicules_controller.rb
@@ -1,2 +1,16 @@
 class VehiculesController < ApplicationController
+
+  def index
+    @vehicules = Vehicule.all
+  end
+
+  private
+
+  def vehicule_params
+    params.require(:vehicule).permit(:location, :brand, :model, :mileage, :price_per_day, :transmission, :category)
+  end
+
+  def set_vehicule
+    @vehicule = Vehicule.find(params[:id])
+  end
 end
