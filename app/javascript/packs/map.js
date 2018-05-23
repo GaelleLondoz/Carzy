93e779d7066737f0ba4f0f45120176be5837f43b
joy navi <joy.navi@gmail.com>
2018-05-23 15:06:15 -0400

    Index without Perfect Layout

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,4 +1,4 @@
-import GMaps from 'gmaps/gmaps.js';
+import GMaps from 'gmaps';
 
 const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
