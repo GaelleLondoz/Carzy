7c427ae75c5f1b6da9b1010422978b4723311d5f
joy navi <joy.navi@gmail.com>
2018-05-23 17:06:05 -0400

    Filters work

--- db/schema.rb
+++ db/schema.rb
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2018_05_23_144041) do
     t.string "category"
     t.string "photo"
     t.string "location"
+    t.text "description"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
